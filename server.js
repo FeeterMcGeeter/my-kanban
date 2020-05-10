75752db6d755ef6f08d47826fc7e7dc82595275c
Jeffrey Davis <davisj0214@yahoo.com>
2020-05-10 05:56:57 -0500

    created react app inside client

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // SET STATIC FOLDER
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 // Connecting to server
 const PORT = process.env.PORT || 5000;
 
